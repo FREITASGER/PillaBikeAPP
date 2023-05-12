cb97476e8edfd0e1f5de559af1303dbb308251f0
GermanGit13 <a26318@svalero.com>
2023-05-12 10:39:20 +0200

    ActionBar - MainActivity y methods API

--- app/src/main/java/com/example/pillabikeapp/api/PillaBikeApiInterface.java
+++ app/src/main/java/com/example/pillabikeapp/api/PillaBikeApiInterface.java
@@ -1,7 +1,34 @@
 package com.example.pillabikeapp.api;
 
+import com.example.pillabikeapp.model.Bike;
+import com.example.pillabikeapp.model.Parking;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
 /**
  * Para definir las operaciones que queremos dar visibilidad en nuestro Aplicacion android provenientes de la API
  */
 public interface PillaBikeApiInterface {
+
+    /**
+     * Parking
+     */
+    @GET("parkings")
+    Call<List<Parking>> getParkings(); //devuelve una lista de parkings
+
+    @GET("parkings/{parkingId}")
+    Call<Parking> getPakingId(@Path("parkingId") long parkingId); //Devuelve un parking por Id
+
+    /**
+     * Bikes
+     */
+    @GET("bikes")
+    Call<List<Bike>> getBikes(); //devuelve una lista de bikes
+
+    @GET("bikes/{bikeId}")
+    Call<Bike> getBikeId(@Path("bikeId") long bikeId); //Devuelve una bike por Id
 }
---
+++ app/src/main/java/com/example/pillabikeapp/contract/ParkingListContract.java
@@ -0,0 +1,35 @@
+package com.example.pillabikeapp.contract;
+
+
+import com.example.pillabikeapp.model.Parking;
+
+import java.util.List;
+
+/**
+ * Declaramos la lógica las view y presenter que une ambas
+ */
+public class ParkingListContract {
+
+    /**
+     * Que necesita el model para solicitar a la API
+     */
+    interface Model {
+        interface OnLoadParkingListener {
+            void onLoadParkingSucess(List<Parking> parkings); //devolvemos una lista de parkings
+            void onLoadParkingError(String message); //Devolvemos un posible error
+        }
+        void loadAllParking( OnLoadParkingListener listener); //quien le llame tiene que tener un listener para que le chive lo que ha pasado
+    }
+
+    /**
+     * Que recibe la view después de solicitarle al presenter
+     */
+    interface View {
+        void showParking(List<Parking> parkings);
+        void showMessage(String message);
+    }
+
+    interface Presenter {
+        void loadAllUsers();
+    }
+}
